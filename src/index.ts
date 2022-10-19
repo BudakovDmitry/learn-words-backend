9b83d360df5bb5f00ea0176c3e04e795e49416df
Budakov Dmitry <budakov.it@gmail.com>
2022-10-20 00:52:35 +0300

    Update procfile

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import AppRouter from './routes/index.js'
 
-// const PORT = 5000
 
 const DB_URL =
   'mongodb+srv://budakovdmitry:1122334455@cluster0.zqbjuh4.mongodb.net/?retryWrites=true&w=majority';
@@ -13,7 +12,7 @@ const router = new AppRouter(app)
 
 
 app.use(cors())
-// app.set('port', )
+app.set("port", process.env.PORT || 5000);
 app.use(express.json())
 
 router.init()
@@ -21,7 +20,8 @@ router.init()
 const startApp = async () => {
   try {
     await mongoose.connect(DB_URL);
-    app.listen(process.env.PORT || 5000, () => console.log('SERVER STARTED ON PORT '));
+    const port = app.get("port");
+    app.listen(port, () => console.log('SERVER STARTED ON PORT ' + port));
     console.log('MongoDB Connected...');
   } catch (error) {
     console.log(error);
